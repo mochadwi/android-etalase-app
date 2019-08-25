04ffb6dbf286875bc047e29465ac68f21d4a9a8a
mochadwi <moch.iqbal@dana.id>
2019-08-25 23:17:08 +0700

    feat(data): Add entity fts-support
    1. Support movie query with fts local db

--- app/src/main/kotlin/io/mochadwi/data/datasource/local/room/MovieFts.kt
+++ app/src/main/kotlin/io/mochadwi/data/datasource/local/room/MovieFts.kt
@@ -6,6 +6,7 @@ import androidx.room.Fts4
 @Fts4(contentEntity = MovieEntity::class)
 @Entity(tableName = "tbl_movie_fts")
 data class MovieFts(
-        val title: String = "", // at nam consequatur ea labore ea harum
-        val body: String = "" // cupiditate quo est a modi nesciunt solutaipsa voluptas error itaque dicta inautem qui minus magnam et distinctio eumaccusamus ratione error aut
+    val title: String = "", // at nam consequatur ea labore ea harum
+    val overview: String,
+    val originalTitle: String = "" // cupiditate quo est a modi nesciunt solutaipsa voluptas error itaque dicta inautem qui minus magnam et distinctio eumaccusamus ratione error aut
 )
