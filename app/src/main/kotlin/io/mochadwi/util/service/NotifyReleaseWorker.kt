b6c97aa032f8a0344955aa8a7a281287e9bd15a7
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2019-11-10 20:45:31 +0700

    feat(release): Enable work mangaer
    - Provide work manager instance in module
    - Provide notify daily & release worker
    - Handle repository when getting the latest movie to discover
    - Check for the valid current date or not, to show as a notification dialog

---
+++ app/src/main/kotlin/io/mochadwi/util/service/NotifyReleaseWorker.kt
@@ -0,0 +1,66 @@
+package io.mochadwi.util.service
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import io.mochadwi.domain.model.movie.Movie
+import io.mochadwi.domain.repository.AppRepository
+import io.mochadwi.util.helper.AppHelper.Const.EXTRA_MOVIE_TITLE
+import io.mochadwi.util.helper.AppHelper.Const.TAG_MOVIE_RELEASE
+import io.mochadwi.util.helper.AppHelper.Strings.getTimeEqualToday
+import org.joda.time.DateTime
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+
+class NotifyReleaseWorker(context: Context, workerParameters: WorkerParameters) :
+        Worker(context, workerParameters), KoinComponent {
+
+    private var mMovies = mutableListOf<Movie>()
+    private var isSucceed = Result.success()
+    private val repo: AppRepository by inject()
+
+    override fun doWork(): Result =
+            when (DateTime().hourOfDay) {
+                8 -> {
+                    Log.d(TAG_MOVIE_RELEASE, "date: " + getTimeEqualToday("2018-10-10"))
+                    getDiscoverMovies("in")
+                }
+                else -> {
+                    Log.d(TAG_MOVIE_RELEASE, "worker retry")
+                    Result.retry()
+                }
+            }
+
+    private fun getDiscoverMovies(lang: String): Result {
+        val list = repo.getDiscoverMovies() ?: emptyList()
+        mMovies.clear()
+
+        when {
+            list.isEmpty() -> isSucceed = Result.retry()
+            else -> {
+                mMovies.addAll(list.filter {
+                    applicationContext.startService(
+                            Intent(applicationContext, NotificationReleaseService::class.java).apply {
+                                putExtra(EXTRA_MOVIE_TITLE, it.originalTitle)
+                            }
+                    )
+
+                    getTimeEqualToday(it.releaseDate)
+                })
+
+                isSucceed = Result.success()
+            }
+        }
+
+        Log.d(TAG_MOVIE_RELEASE, "$isSucceed")
+
+        return isSucceed
+    }
+
+    companion object {
+
+        private val TAG = NotifyReleaseWorker::class.java.simpleName
+    }
+}
